7bc2dd3a704ecaf5c4f348f215524ffc281b0b82
SilverCoin00 <dang.nguyen.sp@gmail.com>
2026-06-21 11:35:52 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], # Trong thực tế nên để URL của frontend, VD: "http://localhost:3000"
+    allow_origins=["https://bkts-tinh-diem-frontend.vercel.app"], # Trong thực tế nên để URL của frontend, VD: "http://localhost:3000"
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
